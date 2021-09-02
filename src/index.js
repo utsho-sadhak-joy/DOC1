1384307fcb15b7a1714558144c30d77c74b243e4
joy <utsho233@gmail.com>
2021-09-02 18:24:19 -0500

    presenting name

--- src/index.js
+++ src/index.js
@@ -2,40 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
 
+
+
+
+const name = <h1>Sadia Rahaman</h1>
+
+ReactDOM.render(name, document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const name = 'Sadia';
 // const joy = <h1>I love you {200*2 } </h1>;
 
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
+// const user = {
+//   firstName: 'Harper',
+//   lastName: 'Perez'
+// };
 
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
-};
+// const element = (
+//   <h1>
+//     Hello, {formatName(user)}!
+//   </h1>
+// );
 
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-);
-
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
-
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
 
 
 
 
-function getGreeting(user) {
-  if (user) {
-    return <h1>Hello, {formatName(user)}!</h1>;
-  }
-  return <h1>Hello, Stranger.</h1>;
-}
+
+// function getGreeting(user) {
+//   if (user) {
+//     return <h1>Hello, {formatName(user)}!</h1>;
+//   }
+//   return <h1>Hello, Stranger.</h1>;
+// }
 
 
 
