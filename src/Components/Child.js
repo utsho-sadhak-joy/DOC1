4719eb5a1c78b30d5ab245f0be5931432713b1eb
joy <utsho233@gmail.com>
2021-09-02 17:30:26 -0500

    Hello World

--- src/Components/Child.js
+++ src/Components/Child.js
@@ -1,14 +1,14 @@
-import React, {useState} from 'react'
-// import Seed
-const Child = () => {
-    const [value, setValue] = useState(1);
-    return (
-        <div>
-            <p>{value}</p>
-            <button onClick={()=>setValue(value+1)}>click here</button>
-        </div>
+// import React, {useState} from 'react'
+// // import Seed
+// const Child = () => {
+//     const [value, setValue] = useState(1);
+//     return (
+//         <div>
+//             <p>{value}</p>
+//             <button onClick={()=>setValue(value+1)}>click here</button>
+//         </div>
          
-    )
-}
+//     )
+// }
 
-export default Child
+// export default Child
--- src/index.js
+++ src/index.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-
+// import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <h1>Hello World!</h1>,
   document.getElementById('root')
+
 );
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
