d1dc9df51331545e696c0edf43a0eae7708a0525
joy <utsho233@gmail.com>
2021-09-02 16:20:50 -0500

    do not know what happen

--- src/Components/Child.js
+++ src/Components/Child.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import sadia from './Img/profile_pic.png'
+import React, {useState} from 'react'
+// import Seed
 const Child = () => {
+    const [value, setValue] = useState(1);
     return (
         <div>
-            <img style={{height:"375px" }} src={sadia}  alt="sadia"  /> 
-            <div>@_S_a_d_i_a_ is her tiktok username;</div>       
+            <p>{value}</p>
+            <button onClick={()=>setValue(value+1)}>click here</button>
         </div>
          
     )
