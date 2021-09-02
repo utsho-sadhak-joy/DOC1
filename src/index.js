f2e7b01d5ead75120d7e14e159961b9f62965439
joy <utsho233@gmail.com>
2021-09-02 17:53:46 -0500

    making name of user 2

--- src/index.js
+++ src/index.js
@@ -5,33 +5,71 @@ import ReactDOM from 'react-dom';
 // const name = 'Sadia';
 // const joy = <h1>I love you {200*2 } </h1>;
 
-function makingName(user){
-  return user.firstName + ' ' + user.lastName;
+
+function defination(para){
+  return para.love + '' + para.sexy;
 }
 
-const user = {
-  firstName: 'Sadia',
-  lastName: 'Rahman',
+
+
+const para = {
+  love : 'Sadia',
+  sexy : 'kiss',
+
 };
 
 
-const element = (
-  <div>
-      {makingName(user)}
-  </div>
-  );
-
-
+const waw = (
+  <h1>{defination(para)}</h1>
+);
 
 
 
 
 
 ReactDOM.render(
-  <h1>{element}</h1>,
+  waw,
   document.getElementById('root')
-
 );
+
+
+
+
+
+
+
+
+
+
+
+
+// function makingName(user){
+//   return user.firstName + ' ' + user.lastName;
+// }
+
+// const user = {
+//   firstName: 'Sadia',
+//   lastName: 'Rahman',
+// };
+
+
+// const element = (
+//   <div>
+//       {makingName(user)}
+//   </div>
+//   );
+
+
+
+
+
+
+
+// ReactDOM.render(
+//   <h1>{element}</h1>,
+//   document.getElementById('root')
+
+// );
 
 
 
