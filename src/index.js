b540490b36ebeaf0febee71c9caebc59f167b54c
joy <utsho233@gmail.com>
2021-09-02 22:23:23 -0500

    name4

--- src/index.js
+++ src/index.js
@@ -134,33 +134,33 @@ import ReactDOM from 'react-dom';
 
 
 
-// function tick() {
-//   const element = (
-//     <div>
-//       <h1>Hello, world!</h1>
-//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
-//     </div>
-//   );
-//   ReactDOM.render(element, document.getElementById('root'));
-// }
-
-// setInterval(tick, 1000);
-
-
-
-
-
-function tick(){
+function tick() {
   const element = (
-    <div> 
-      <h1>Hello World!</h1>
-      <p>It is  {new Date().toLocaleTimeString()} </p>
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
-  ReactDOM.render(element,document.getElementById('root'),);
-};
+  ReactDOM.render(element, document.getElementById('root'));
+}
 
-setInterval(tick,100)
+setInterval(tick, 1000);
+
+
+
+
+
+// function tick(){
+//   const element = (
+//     <div> 
+//       <h1>Hello World!</h1>
+//       <p>It is  {new Date().toLocaleTimeString()} </p>
+//     </div>
+//   );
+//   ReactDOM.render(element,document.getElementById('root'),);
+// };
+
+// setInterval(tick,100)
 
 
 
