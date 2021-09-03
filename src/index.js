0456f866b53ca046a32de3ff398662c09254dfd5
joy <utsho233@gmail.com>
2021-09-02 22:25:14 -0500

    name9

--- src/index.js
+++ src/index.js
@@ -84,19 +84,19 @@ import ReactDOM from 'react-dom';
 
 
 
-// class Welcome extends React.Component{
-//   render() {
-//     return <h1>Hi {this.props.name}</h1>
-//   }
-// }
+class Welcome extends React.Component{
+  render() {
+    return <h1>Hi {this.props.name}</h1>
+  }
+}
 
 
-const element = <Welcome name="Sharmistah"/>;
+// const element = <Welcome name="Sharmistah"/>;
 
 
-ReactDOM.render(
-  element, document.getElementById('root')
-);
+// ReactDOM.render(
+//   element, document.getElementById('root')
+// );
 
 // 11111111111111111111111111111111111111111111111111111111111111111111111111111111111
 
