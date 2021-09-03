fc95516c67b5d2abba2fce57882ea35398a1b507
joy <utsho233@gmail.com>
2021-09-02 20:10:26 -0500

    Clock

--- src/index.js
+++ src/index.js
@@ -5,42 +5,39 @@ import ReactDOM from 'react-dom';
 
 
 
-const name = <h1>Sadia Rahaman</h1>
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
 
-ReactDOM.render(name, document.getElementById('root'));
-
-
-
-
-
-
-
-
-
-
+// setInterval(tick, 1000);
 
 
 
 
 
+function tick(){
+  const element = (
+    <div> 
+      <h1>Hello World!</h1>
+      <p>It is  {new Date().toLocaleTimeString()} </p>
+    </div>
+  );
+  ReactDOM.render(element,document.getElementById('root'),);
+};
+
+setInterval(tick,100)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// const name = <h1>Sadia Rahaman</h1>
+// ReactDOM.render(name, document.getElementById('root'));
 
 
 
