0831b919a16e3c331e795bff9dadc43d603649c8
joy <utsho233@gmail.com>
2021-09-06 09:31:12 +0600

    successfully completed the clock

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import React from 'react';
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 // import App from './App';
 
 
@@ -10,21 +10,39 @@
 
 import React from 'react'
 
-export default class index extends React.Component {
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+
+  componentDidMount() {
+    this.timerId = setInterval(() =>this.tick(), 1000);
+
+  };
+
+  componentWillUnmount() {
+      clearInterval(this.timerId);
+  };
+
+
+  tick () {
+    this.setState({date: new Date()});
+  }
+
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
 }
 
 
-
-
-
+ReactDOM.render(<Clock />, document.getElementById('root'))
 
 
 
