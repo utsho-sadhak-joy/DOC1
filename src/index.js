39a0eed0319ea8447bbca7f7b2554e2639401350
joy <utsho233@gmail.com>
2021-09-06 10:28:50 +0600

    problem runing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
 
@@ -7,42 +7,15 @@ import ReactDOM from 'react-dom';
 
 
 
-
-import React from 'react'
-
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-
-  componentDidMount() {
-    this.timerId = setInterval(() =>this.tick(), 1000);
-
+constructor(props) {
+  super(props);
+  this.state = {
+    posts: [],
+    comments: []
   };
-
-  componentWillUnmount() {
-      clearInterval(this.timerId);
-  };
-
-
-  tick () {
-    this.setState({date: new Date()});
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
 }
 
 
-ReactDOM.render(<Clock />, document.getElementById('root'))
 
 
 
@@ -50,38 +23,19 @@ ReactDOM.render(<Clock />, document.getElementById('root'))
 
 
 
+componentDidMount() {
+  fetchPosts().then(response => {
+    this.setState({
+      posts: response.posts
+    });
+  });
 
-
-
-this.setState((state,props)=>({
-  counter: state.counter + props.increment
-}));
-
-
-
-this.setState((state,props)=>({
-  counter: state.counter + props.increment
-}));
-
-
-
-
-
-
-this.setState(function(state,props){
-  return{
-    Counter: state.counter + props.increment
-  };
-});
-
-
-
-
-
-
-
-
-
+  fetchComments().then(response => {
+    this.setState({
+      comments: response.comments
+    });
+  });
+}
 
 
 
