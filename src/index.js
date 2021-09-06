02a5c87d35f37773baa445756978188b528668ea
joy <utsho233@gmail.com>
2021-09-06 10:29:21 +0600

    clear

--- src/index.js
+++ src/index.js
@@ -1,41 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 // import App from './App';
 
 
 
 
 
-
-constructor(props) {
-  super(props);
-  this.state = {
-    posts: [],
-    comments: []
-  };
-}
-
-
-
-
-
-
-
-
-
-componentDidMount() {
-  fetchPosts().then(response => {
-    this.setState({
-      posts: response.posts
-    });
-  });
-
-  fetchComments().then(response => {
-    this.setState({
-      comments: response.comments
-    });
-  });
-}
 
 
 
