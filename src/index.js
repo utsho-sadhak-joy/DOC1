cce4721925b89e1707bae7952caf48a8dda5696c
joy <utsho233@gmail.com>
2021-10-11 08:51:55 +0600

    accebality2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,39 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
+import  { Fragment } from 'react';
+
+function ListItem({ item }) {
+  return (
+    <Fragment>
+      <dt>{item.term}</dt>
+      <dd>{item.description}</dd>
+    </Fragment>
+  );
+}
+
+function Glossary(props) {
+  return (
+    <dl>
+      {props.items.map(item => (
+        // Fragments should also have a `key` prop when mapping collections
+        <Fragment key={item.id}>
+          <dt>{item.term}</dt>
+          <dd>{item.description}</dd>
+        </Fragment>
+      ))}
+    </dl>
+  );
+}
+
+export default Glossary
+
+
+
+
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
