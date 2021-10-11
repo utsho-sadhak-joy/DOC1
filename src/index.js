97e3d87df99321061709b465986103579a514828
joy <utsho233@gmail.com>
2021-10-11 09:09:01 +0600

    end od accebality

--- src/index.js
+++ src/index.js
@@ -3,28 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
-import  { Fragment } from 'react';
-
-// function ListItem({ item }) {
-//   return (
-//     <Fragment>
-//       <dt>{item.term}</dt>
-//       <dd>{item.description}</dd>
-//     </Fragment>
-//   );
-// }
-
-<label htmlFor="namedInput">Name:</label>
-<input id="namedInput" type="text" name="name"/>
-
-
-
-
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <ListItem />
   </React.StrictMode>,
   document.getElementById('root')
 );
