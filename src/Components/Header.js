e5418d7e36953fd39bb5c6c43e9328b20444a653
joy <utsho233@gmail.com>
2021-11-02 21:53:52 +0600

    hoc-clear

--- src/Components/Header.js
+++
@@ -1,9 +0,0 @@
-// Instead of...
-MyComponent.someFunction = someFunction;
-export default MyComponent;
-
-// ...export the method separately...
-export { someFunction };
-
-// ...and in the consuming module, import both
-import MyComponent, { someFunction } from './MyComponent.js';
