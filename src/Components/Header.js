8ec33ab4261984506e803a15e43f6ce44fe7da34
joy <utsho233@gmail.com>
2021-11-02 21:44:17 +0600

    hoc8

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,17 @@
-function logProps(WrappedComponent) {
-    return class extends React.Component {
-      componentDidUpdate(prevProps) {
-        console.log('Current props: ', this.props);
-        console.log('Previous props: ', prevProps);
-      }
-      render() {
-        // Wraps the input component in a container, without mutating it. Good!
-        return <WrappedComponent {...this.props} />;
-      }
-    }
+render() {
+    // Filter out extra props that are specific to this HOC and shouldn't be
+    // passed through
+    const { extraProp, ...passThroughProps } = this.props;
+  
+    // Inject props into the wrapped component. These are usually state values or
+    // instance methods.
+    const injectedProp = someStateOrInstanceMethod;
+  
+    // Pass props to wrapped component
+    return (
+      <WrappedComponent
+        injectedProp={injectedProp}
+        {...passThroughProps}
+      />
+    );
   }
