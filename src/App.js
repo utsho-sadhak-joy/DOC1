36a04bc02f1b7e3d6f40dcccfd7315f57bd58c64
joy <utsho233@gmail.com>
2021-11-02 21:39:43 +0600

    hoc5

--- src/App.js
+++ src/App.js
@@ -28,37 +28,15 @@ function Glossary(props) {
 
 
 
-// This function takes a component...
-function withSubscription(WrappedComponent, selectData) {
-  // ...and returns another component...
-  return class extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleChange = this.handleChange.bind(this);
-      this.state = {
-        data: selectData(DataSource, props)
-      };
-    }
-
-    componentDidMount() {
-      // ... that takes care of the subscription...
-      DataSource.addChangeListener(this.handleChange);
-    }
-
-    componentWillUnmount() {
-      DataSource.removeChangeListener(this.handleChange);
-    }
-
-    handleChange() {
-      this.setState({
-        data: selectData(DataSource, this.props)
-      });
-    }
-
-    render() {
-      // ... and renders the wrapped component with the fresh data!
-      // Notice that we pass through any additional props
-      return <WrappedComponent data={this.state.data} {...this.props} />;
-    }
+function logProps(InputComponent) {
+  InputComponent.prototype.componentDidUpdate = function(prevProps) {
+    console.log('Current props: ', this.props);
+    console.log('Previous props: ', prevProps);
   };
+  // The fact that we're returning the original input is a hint that it has
+  // been mutated.
+  return InputComponent;
 }
+
+// EnhancedComponent will log whenever props are received
+const EnhancedComponent = logProps(InputComponent);
