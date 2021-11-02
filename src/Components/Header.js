89e4ddadc1cacb80ac172fbd368056ab09c1f510
joy <utsho233@gmail.com>
2021-11-02 21:49:23 +0600

    hoc15

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,8 @@
 render() {
-    // Filter out extra props that are specific to this HOC and shouldn't be
-    // passed through
-    const { extraProp, ...passThroughProps } = this.props;
-  
-    // Inject props into the wrapped component. These are usually state values or
-    // instance methods.
-    const injectedProp = someStateOrInstanceMethod;
-  
-    // Pass props to wrapped component
-    return (
-      <WrappedComponent
-        injectedProp={injectedProp}
-        {...passThroughProps}
-      />
-    );
+    // A new version of EnhancedComponent is created on every render
+    // EnhancedComponent1 !== EnhancedComponent2
+    const EnhancedComponent = enhance(MyComponent);
+    // That causes the entire subtree to unmount/remount each time!
+    return <EnhancedComponent />;
   }
-
-
-  React’s diffing algorithm (called Reconciliation) uses component identity to determine whether it should update the existing subtree or throw it away and mount a new one. If the component returned from render is identical (===) to the component from the previous render, React recursively updates the subtree by diffing it with the new one. If they’re not equal, the previous subtree is unmounted completely.
+  
