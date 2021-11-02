b1651b909a16d1a48a068f7df76c9c27f2d44dbb
joy <utsho233@gmail.com>
2021-11-02 21:42:38 +0600

    hoc7

--- src/App.js
+++ src/App.js
@@ -10,20 +10,3 @@ function App() {
 
 export default App;
 
-function Glossary(props) {
-  return (
-    <dl>
-      {props.items.map(item => (
-        // Without the `key`, React will fire a key warning
-        <React.Fragment key={item.id}>
-          <dt>{item.term}</dt>
-          <dd>{item.description}</dd>
-        </React.Fragment>
-      ))}
-    </dl>
-  );
-}
-
-
-
-
