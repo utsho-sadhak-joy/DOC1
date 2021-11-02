c744dd320427d6024660f3d3380933f87b855834
joy <utsho233@gmail.com>
2021-11-02 21:27:10 +0600

    frag6

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ export default App;
 class Columns extends React.Component {
   render() {
     return (
-      <React.Fragment>
+      <>
         <td>Hello</td>
         <td>World</td>
-      </React.Fragment>
+      </>
     );
   }
 }
