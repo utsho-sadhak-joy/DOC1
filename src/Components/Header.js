b8c3d85ce278907523fc4786e409643559060254
joy <utsho233@gmail.com>
2021-11-03 09:31:02 +0600

    jsx3

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,4 +7,18 @@ function App1() {
     return <Greeting {...props} />;
   }
 
+  const Button = props => {
+    const { kind, ...other } = props;
+    const className = kind === "primary" ? "PrimaryButton" : "SecondaryButton";
+    return <button className={className} {...other} />;
+  };
   
+  const App = () => {
+    return (
+      <div>
+        <Button kind="primary" onClick={() => console.log("clicked!")}>
+          Hello World!
+        </Button>
+      </div>
+    );
+  }
