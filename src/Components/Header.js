ab5755b822f6beef90daa88a7867550467eaaad7
joy <utsho233@gmail.com>
2021-11-03 09:12:39 +0600

    no-updat4

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,5 @@
-componentDidMount() {
-    this.$el = $(this.el);
-    this.$el.chosen();
-  
-    this.handleChange = this.handleChange.bind(this);
-    this.$el.on('change', this.handleChange);
-  }
-  
-  componentWillUnmount() {
-    this.$el.off('change', this.handleChange);
-    this.$el.chosen('destroy');
-  }
-  
-  handleChange(e) {
-    this.props.onChange(e.target.value);
+componentDidUpdate(prevProps) {
+    if (prevProps.children !== this.props.children) {
+      this.$el.trigger("chosen:updated");
+    }
   }
