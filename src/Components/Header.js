b5ca39cd8b0dd9e4758ca9b361488aa769ddde78
joy <utsho233@gmail.com>
2021-11-03 09:19:39 +0600

    no-upda9

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -86,3 +86,55 @@ function Button() {
       Backbone.View.prototype.remove.call(this);
     }
   });
+
+  class Item extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+    }
+  
+    handleChange() {
+      this.forceUpdate();
+    }
+  
+    componentDidMount() {
+      this.props.model.on('change', this.handleChange);
+    }
+  
+    componentWillUnmount() {
+      this.props.model.off('change', this.handleChange);
+    }
+  
+    render() {
+      return <li>{this.props.model.get('text')}</li>;
+    }
+  }
+  
+  class List extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+    }
+  
+    handleChange() {
+      this.forceUpdate();
+    }
+  
+    componentDidMount() {
+      this.props.collection.on('add', 'remove', this.handleChange);
+    }
+  
+    componentWillUnmount() {
+      this.props.collection.off('add', 'remove', this.handleChange);
+    }
+  
+    render() {
+      return (
+        <ul>
+          {this.props.collection.map(model => (
+            <Item key={model.cid} model={model} />
+          ))}
+        </ul>
+      );
+    }
+  }
