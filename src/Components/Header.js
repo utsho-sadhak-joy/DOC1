08f24e255028c53b5b653e276d3fda2e6e6b59b3
joy <utsho233@gmail.com>
2021-11-03 12:04:43 +0600

    re10

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -91,3 +91,20 @@ class Mouse extends React.PureComponent {
       );
     }
   }
+
+  class MouseTracker extends React.Component {
+    // Defined as an instance method, `this.renderTheCat` always
+    // refers to *same* function when we use it in render
+    renderTheCat(mouse) {
+      return <Cat mouse={mouse} />;
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1>Move the mouse around!</h1>
+          <Mouse render={this.renderTheCat} />
+        </div>
+      );
+    }
+  }
