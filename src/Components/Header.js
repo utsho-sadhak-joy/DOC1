c9a82dc378937c090b989ac12bfbd39b6c9ff2ed
joy <utsho233@gmail.com>
2021-11-03 11:54:49 +0600

    re4

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,39 @@
-function MyFunctionComponent() {
-    return <input />;
-  }
-  
-  class Parent extends React.Component {
+class CustomTextInput extends React.Component {
     constructor(props) {
       super(props);
-      this.textInput = React.createRef();
+  
+      this.textInput = null;
+  
+      this.setTextInputRef = element => {
+        this.textInput = element;
+      };
+  
+      this.focusTextInput = () => {
+        // Focus the text input using the raw DOM API
+        if (this.textInput) this.textInput.focus();
+      };
     }
+  
+    componentDidMount() {
+      // autofocus the input on mount
+      this.focusTextInput();
+    }
+  
     render() {
-      // This will *not* work!
+      // Use the `ref` callback to store a reference to the text input DOM
+      // element in an instance field (for example, this.textInput).
       return (
-        <MyFunctionComponent ref={this.textInput} />
+        <div>
+          <input
+            type="text"
+            ref={this.setTextInputRef}
+          />
+          <input
+            type="button"
+            value="Focus the text input"
+            onClick={this.focusTextInput}
+          />
+        </div>
       );
     }
   }
