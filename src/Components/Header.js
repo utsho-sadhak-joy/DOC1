09642208f9edbf3b50578d0a7773023cf3bf278e
joy <utsho233@gmail.com>
2021-11-03 09:21:12 +0600

    no-upda10

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -138,3 +138,42 @@ function Button() {
       );
     }
   }
+
+
+
+
+  function connectToBackboneModel(WrappedComponent) {
+    return class BackboneComponent extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = Object.assign({}, props.model.attributes);
+        this.handleChange = this.handleChange.bind(this);
+      }
+  
+      componentDidMount() {
+        this.props.model.on('change', this.handleChange);
+      }
+  
+      componentWillReceiveProps(nextProps) {
+        this.setState(Object.assign({}, nextProps.model.attributes));
+        if (nextProps.model !== this.props.model) {
+          this.props.model.off('change', this.handleChange);
+          nextProps.model.on('change', this.handleChange);
+        }
+      }
+  
+      componentWillUnmount() {
+        this.props.model.off('change', this.handleChange);
+      }
+  
+      handleChange(model) {
+        this.setState(model.changedAttributes());
+      }
+  
+      render() {
+        const propsExceptModel = Object.assign({}, this.props);
+        delete propsExceptModel.model;
+        return <WrappedComponent {...propsExceptModel} {...this.state} />;
+      }
+    }
+  }
