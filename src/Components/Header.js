c27ce68aa4b8be45f7cb024e3a441d266c57dcd9
joy <utsho233@gmail.com>
2021-11-03 09:11:25 +0600

    no-updat3

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,1 +1,16 @@
-<select className="Chosen-select" ref={el => this.el = el}>
+componentDidMount() {
+    this.$el = $(this.el);
+    this.$el.chosen();
+  
+    this.handleChange = this.handleChange.bind(this);
+    this.$el.on('change', this.handleChange);
+  }
+  
+  componentWillUnmount() {
+    this.$el.off('change', this.handleChange);
+    this.$el.chosen('destroy');
+  }
+  
+  handleChange(e) {
+    this.props.onChange(e.target.value);
+  }
