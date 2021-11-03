5489dd20f831a213e6a5e888bcfad78931a41470
joy <utsho233@gmail.com>
2021-11-03 09:27:12 +0600

    no-upda12

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -209,3 +209,28 @@ function Button() {
     <Example model={model} />,
     document.getElementById('root')
   );
+
+  import React from 'react';
+
+const MyComponents = {
+  DatePicker: function DatePicker(props) {
+    return <div>Imagine a {props.color} datepicker here.</div>;
+  }
+}
+
+function BlueDatePicker() {
+  return <MyComponents.DatePicker color="blue" />;
+}
+
+import React from 'react';
+
+// Wrong! This is a component and should have been capitalized:
+function hello(props) {
+  // Correct! This use of <div> is legitimate because div is a valid HTML tag:
+  return <div>Hello {props.toWhat}</div>;
+}
+
+function HelloWorld() {
+  // Wrong! React thinks <hello /> is an HTML tag because it's not capitalized:
+  return <hello toWhat="World" />;
+}
