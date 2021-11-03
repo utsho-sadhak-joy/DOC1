5749a791dc6a76e70433c7631b1e6a4298f2b37e
joy <utsho233@gmail.com>
2021-11-03 09:46:04 +0600

    per1

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -52,3 +52,31 @@ function Repeat(props) {
   shouldComponentUpdate(nextProps, nextState) {
     return true;
   }
+
+
+  class CounterButton extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {count: 1};
+    }
+  
+    shouldComponentUpdate(nextProps, nextState) {
+      if (this.props.color !== nextProps.color) {
+        return true;
+      }
+      if (this.state.count !== nextState.count) {
+        return true;
+      }
+      return false;
+    }
+  
+    render() {
+      return (
+        <button
+          color={this.props.color}
+          onClick={() => this.setState(state => ({count: state.count + 1}))}>
+          Count: {this.state.count}
+        </button>
+      );
+    }
+  }
