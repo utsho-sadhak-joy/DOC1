717273f4dc8d3ad8857fec9d439ecc6302f1004a
joy <utsho233@gmail.com>
2021-11-03 12:03:54 +0600

    re9

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -61,3 +61,33 @@ function withMouse(Component) {
     }
   }
 }
+
+
+
+<Mouse>
+  {mouse => (
+    <p>The mouse position is {mouse.x}, {mouse.y}</p>
+  )}
+</Mouse>
+
+class Mouse extends React.PureComponent {
+    // Same implementation as above...
+  }
+  
+  class MouseTracker extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Move the mouse around!</h1>
+  
+          {/*
+            This is bad! The value of the `render` prop will
+            be different on each render.
+          */}
+          <Mouse render={mouse => (
+            <Cat mouse={mouse} />
+          )}/>
+        </div>
+      );
+    }
+  }
