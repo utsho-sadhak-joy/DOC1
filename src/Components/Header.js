3bd2e3eedafbf068874d8f7e111641c0b503e09c
joy <utsho233@gmail.com>
2021-11-03 12:23:35 +0600

    strict5

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -251,3 +251,22 @@ class MyComponent extends React.Component {
 MyComponent.propTypes = {
   children: PropTypes.element.isRequired
 };
+
+class Greeting extends React.Component {
+    render() {
+      return (
+        <h1>Hello, {this.props.name}</h1>
+      );
+    }
+  }
+  
+  // Specifies the default values for props:
+  Greeting.defaultProps = {
+    name: 'Stranger'
+  };
+  
+  // Renders "Hello, Stranger":
+  ReactDOM.render(
+    <Greeting />,
+    document.getElementById('example')
+  );
