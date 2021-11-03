0d8430664db3c0a239077cec98b404abd66e4826
joy <utsho233@gmail.com>
2021-11-03 11:57:33 +0600

    re5

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,38 +1,22 @@
-class CustomTextInput extends React.Component {
+class MouseTracker extends React.Component {
     constructor(props) {
       super(props);
-  
-      this.textInput = null;
-  
-      this.setTextInputRef = element => {
-        this.textInput = element;
-      };
-  
-      this.focusTextInput = () => {
-        // Focus the text input using the raw DOM API
-        if (this.textInput) this.textInput.focus();
-      };
+      this.handleMouseMove = this.handleMouseMove.bind(this);
+      this.state = { x: 0, y: 0 };
     }
   
-    componentDidMount() {
-      // autofocus the input on mount
-      this.focusTextInput();
+    handleMouseMove(event) {
+      this.setState({
+        x: event.clientX,
+        y: event.clientY
+      });
     }
   
     render() {
-      // Use the `ref` callback to store a reference to the text input DOM
-      // element in an instance field (for example, this.textInput).
       return (
-        <div>
-          <input
-            type="text"
-            ref={this.setTextInputRef}
-          />
-          <input
-            type="button"
-            value="Focus the text input"
-            onClick={this.focusTextInput}
-          />
+        <div style={{ height: '100vh' }} onMouseMove={this.handleMouseMove}>
+          <h1>Move the mouse around!</h1>
+          <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
         </div>
       );
     }
