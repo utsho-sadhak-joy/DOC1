8271edda7c42a9119e70d32eb18438cc963f04be
joy <utsho233@gmail.com>
2021-11-03 12:27:47 +0600

    strict7

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -337,3 +337,52 @@ class NameForm extends React.Component {
       );
     }
   }
+
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            defaultValue="Bob"
+            type="text"
+            ref={this.input} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+
+
+  class FileInput extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.fileInput = React.createRef();
+    }
+    handleSubmit(event) {
+      event.preventDefault();
+      alert(
+        `Selected file - ${this.fileInput.current.files[0].name}`
+      );
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Upload file:
+            <input type="file" ref={this.fileInput} />
+          </label>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <FileInput />,
+    document.getElementById('root')
+  );
