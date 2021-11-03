1741ae723a537f09cc12fc3726a150c3df864f69
joy <utsho233@gmail.com>
2021-11-03 12:18:19 +0600

    strict

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -108,3 +108,19 @@ class Mouse extends React.PureComponent {
       );
     }
   }
+
+  class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.inputRef = React.createRef();
+    }
+  
+    render() {
+      return <input type="text" ref={this.inputRef} />;
+    }
+  
+    componentDidMount() {
+      this.inputRef.current.focus();
+    }
+  }
