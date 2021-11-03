e90bda3d053c27fd54124dddf33f3bb249cbe0dd
joy <utsho233@gmail.com>
2021-11-03 09:27:35 +0600

    no-upda13

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -234,3 +234,16 @@ function HelloWorld() {
   // Wrong! React thinks <hello /> is an HTML tag because it's not capitalized:
   return <hello toWhat="World" />;
 }
+
+import React from 'react';
+
+// Correct! This is a component and should be capitalized:
+function Hello(props) {
+  // Correct! This use of <div> is legitimate because div is a valid HTML tag:
+  return <div>Hello {props.toWhat}</div>;
+}
+
+function HelloWorld() {
+  // Correct! React knows <Hello /> is a component because it's capitalized.
+  return <Hello toWhat="World" />;
+}
