a759c551020105a3d99452f03df2b063ed4e5b2f
joy <utsho233@gmail.com>
2021-11-03 09:16:28 +0600

    no-updat7

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -52,3 +52,20 @@ function Button() {
       });
     }
   );
+
+
+  function Button(props) {
+    return <button onClick={props.onClick}>Say Hello</button>;
+  }
+  
+  function HelloButton() {
+    function handleClick() {
+      alert('Hello!');
+    }
+    return <Button onClick={handleClick} />;
+  }
+  
+  ReactDOM.render(
+    <HelloButton />,
+    document.getElementById('container')
+  );
