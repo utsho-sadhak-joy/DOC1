4efdbcc59b97ba7e6f536e012e6315e5bb2fbb21
joy <utsho233@gmail.com>
2021-11-03 12:31:52 +0600

    strict9

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -395,3 +395,15 @@ class NameForm extends React.Component {
       </brick-flipbox>
     );
   }
+
+  class XSearch extends HTMLElement {
+    connectedCallback() {
+      const mountPoint = document.createElement('span');
+      this.attachShadow({ mode: 'open' }).appendChild(mountPoint);
+  
+      const name = this.getAttribute('name');
+      const url = 'https://www.google.com/search?q=' + encodeURIComponent(name);
+      ReactDOM.render(<a href={url}>{name}</a>, mountPoint);
+    }
+  }
+  customElements.define('x-search', XSearch);
