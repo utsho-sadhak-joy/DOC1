701f8e555cc0ac721ddd5ccb6af6ea978ccc364d
joy <utsho233@gmail.com>
2021-11-03 09:48:26 +0600

    per3

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -80,3 +80,65 @@ function Repeat(props) {
       );
     }
   }
+
+
+  class CounterButton extends React.PureComponent {
+    constructor(props) {
+      super(props);
+      this.state = {count: 1};
+    }
+  
+    render() {
+      return (
+        <button
+          color={this.props.color}
+          onClick={() => this.setState(state => ({count: state.count + 1}))}>
+          Count: {this.state.count}
+        </button>
+      );
+    }
+  }
+
+
+  class ListOfWords extends React.PureComponent {
+    render() {
+      return <div>{this.props.words.join(',')}</div>;
+    }
+  }
+  
+  class WordAdder extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        words: ['marklar']
+      };
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    handleClick() {
+      // This section is bad style and causes a bug
+      const words = this.state.words;
+      words.push('marklar');
+      this.setState({words: words});
+    }
+  
+    render() {
+      return (
+        <div>
+          <button onClick={this.handleClick} />
+          <ListOfWords words={this.state.words} />
+        </div>
+      );
+    }
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      words: state.words.concat(['marklar'])
+    }));
+  }
+  handleClick() {
+    this.setState(state => ({
+      words: state.words.concat(['marklar'])
+    }));
+  }
