d7b09f7e33054ee770705f4395cb5e6ff7b75448
joy <utsho233@gmail.com>
2021-11-03 12:25:54 +0600

    strict6

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -270,3 +270,70 @@ class Greeting extends React.Component {
     <Greeting />,
     document.getElementById('example')
   );
+
+
+  class Greeting extends React.Component {
+    static defaultProps = {
+      name: 'stranger'
+    }
+  
+    render() {
+      return (
+        <div>Hello, {this.props.name}</div>
+      )
+    }
+  }
+
+  export default function HelloWorldComponent({ name }) {
+    return (
+      <div>Hello, {name}</div>
+    )
+  }
+  function HelloWorldComponent({ name }) {
+    return (
+      <div>Hello, {name}</div>
+    )
+  }
+  
+  export default HelloWorldComponent
+
+
+  import PropTypes from 'prop-types'
+
+function HelloWorldComponent({ name }) {
+  return (
+    <div>Hello, {name}</div>
+  )
+}
+
+HelloWorldComponent.propTypes = {
+  name: PropTypes.string
+}
+
+export default HelloWorldComponent
+
+
+class NameForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.input = React.createRef();
+    }
+  
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.input.current.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" ref={this.input} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+  }
