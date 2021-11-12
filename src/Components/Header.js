f753fee260b86682b4eae141acc54544c3582745
joy <utsho233@gmail.com>
2021-11-12 10:59:46 +0600

    testutilitis1

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -112,3 +112,32 @@ class Cat extends React.Component {
       </div>
     );
   }
+
+  class Counter extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {count: 0};
+      this.handleClick = this.handleClick.bind(this);
+    }
+    componentDidMount() {
+      document.title = `You clicked ${this.state.count} times`;
+    }
+    componentDidUpdate() {
+      document.title = `You clicked ${this.state.count} times`;
+    }
+    handleClick() {
+      this.setState(state => ({
+        count: state.count + 1,
+      }));
+    }
+    render() {
+      return (
+        <div>
+          <p>You clicked {this.state.count} times</p>
+          <button onClick={this.handleClick}>
+            Click me
+          </button>
+        </div>
+      );
+    }
+  }
