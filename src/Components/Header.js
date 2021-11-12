479675b553b3e19a6ad95d7fc7b4aa62374a158d
joy <utsho233@gmail.com>
2021-11-12 19:07:24 +0600

    test drive2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -191,3 +191,26 @@ console.log(testRenderer.toJSON());
 // { type: 'a',
 //   props: { href: 'https://www.facebook.com/' },
 //   children: [ 'Facebook' ] }
+
+import TestRenderer from 'react-test-renderer';
+
+function MyComponent() {
+  return (
+    <div>
+      <SubComponent foo="bar" />
+      <p className="my">Hello</p>
+    </div>
+  )
+}
+
+function SubComponent() {
+  return (
+    <p className="sub">Sub</p>
+  );
+}
+
+const testRenderer = TestRenderer.create(<MyComponent />);
+const testInstance = testRenderer.root;
+
+expect(testInstance.findByType(SubComponent).props.foo).toBe('bar');
+expect(testInstance.findByProps({className: "sub"}).children).toEqual(['Sub']);
