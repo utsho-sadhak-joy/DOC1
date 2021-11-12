eaac75c10c12de554c8f817d0a1ea96583ad2359
joy <utsho233@gmail.com>
2021-11-12 21:20:30 +0600

    hook4

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -34,3 +34,24 @@ function Example() {
     </div>
   );
 }
+
+
+
+function FriendStatusWithCounter(props) {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+  const [isOnline, setIsOnline] = useState(null);
+  useEffect(() => {
+    ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
+    return () => {
+      ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
+    };
+  });
+
+  function handleStatusChange(status) {
+    setIsOnline(status.isOnline);
+  }
+  // ...
