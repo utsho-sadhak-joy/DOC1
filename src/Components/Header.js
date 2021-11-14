e7d4ebf002d59807b37c12988e4562d631117e06
joy <utsho233@gmail.com>
2021-11-14 13:39:22 +0600

    hook15

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -114,3 +114,21 @@ class FriendStatusWithCounter extends React.Component {
       isOnline: status.isOnline
     });
   }
+
+  function FriendStatusWithCounter(props) {
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+      document.title = `You clicked ${count} times`;
+    });
+  
+    const [isOnline, setIsOnline] = useState(null);
+    useEffect(() => {
+      function handleStatusChange(status) {
+        setIsOnline(status.isOnline);
+      }
+  
+      ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
+      return () => {
+        ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
+      };
+    });
