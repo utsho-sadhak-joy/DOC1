b3fe03f4821675f16db777f3b06cb45797ba5d6b
joy <utsho233@gmail.com>
2021-11-15 23:32:46 +0600

    hook8987

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,4 +8,53 @@ function Counter({initialCount}) {
         <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
       </>
     );
-  }
+  }const [state, setState] = useState({});
+  setState(prevState => {
+    // Object.assign would also work
+    return {...prevState, ...updatedValues};
+  });
+  function Counter({initialCount}) {
+    const [count, setCount] = useState(initialCount);
+    return (
+      <>
+        Count: {count}
+        <button onClick={() => setCount(initialCount)}>Reset</button>
+        <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+        <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+      </>
+    );
+  }const [state, setState] = useState({});
+  setState(prevState => {
+    // Object.assign would also work
+    return {...prevState, ...updatedValues};
+  });
+  function Counter({initialCount}) {
+    const [count, setCount] = useState(initialCount);
+    return (
+      <>
+        Count: {count}
+        <button onClick={() => setCount(initialCount)}>Reset</button>
+        <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+        <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+      </>
+    );
+  }const [state, setState] = useState({});
+  setState(prevState => {
+    // Object.assign would also work
+    return {...prevState, ...updatedValues};
+  });
+  function Counter({initialCount}) {
+    const [count, setCount] = useState(initialCount);
+    return (
+      <>
+        Count: {count}
+        <button onClick={() => setCount(initialCount)}>Reset</button>
+        <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+        <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+      </>
+    );
+  }const [state, setState] = useState({});
+  setState(prevState => {
+    // Object.assign would also work
+    return {...prevState, ...updatedValues};
+  });
